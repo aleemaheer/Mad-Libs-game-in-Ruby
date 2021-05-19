bd8c59b1a6078cf8f318f2095cd0f3241835689a
Aleem Aheer <aleemaheer5769644@gmail.com>
2021-05-19 22:25:08 +0500

    Initial commit

---
+++ game.rb
@@ -0,0 +1,14 @@
+# making a mad libs game in ruby
+
+print "Enter your name: "
+yourname = gets.chomp()
+print "Enter a celebrity name: "
+celebrityname = gets
+print "Enter your friend's name: "
+friendname = gets
+
+# printing some interesting results
+
+print (yourname)
+print (" loves " + celebrityname)
+print ("But you hates " + friendname)
